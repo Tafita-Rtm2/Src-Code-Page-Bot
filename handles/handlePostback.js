eea4c3ee2d53d6537f865eeea4aca4d2115af403
Tafitaniaina rtm <harenabainina@gmail.com>
2025-04-27 17:54:44 +0300

    Update handlePostback.js

--- handles/handlePostback.js
+++ handles/handlePostback.js
@@ -4,10 +4,15 @@ const handlePostback = async (event, pageAccessToken) => {
   const { id: senderId } = event.sender || {};
   const { payload } = event.postback || {};
 
-  if (!senderId || !payload) return console.error('Invalid postback event object');
+  if (!senderId || !name) return console.error('Invalid postback event object');
 
   try {
-    await sendMessage(senderId, { text: `You sent a postback with payload: ${payload}` }, pageAccessToken);
+    await sendMessage(senderId, { text: `"🎉 Bienvenue sur "Bot Traduction Rtm" ! 🤖 Je suis ravi de vous aider à traduire votre texte, votre phrase ou votre mot dans la langue de votre choix 🔍
+
+Pour commencer, n'hésitez pas à saisir le texte, la phrase ou le mot que vous souhaitez que je traduise et je ferai de mon mieux pour vous aider 📝
+
+Tapez, et commençons à traduire ! 💬"
+: ${name}` }, pageAccessToken);
   } catch (err) {
     console.error('Error sending postback response:', err.message || err);
   }
